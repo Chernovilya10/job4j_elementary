85324ebc08b064b92d28be31eac6201e6e33e339
Chernovilya10 <i.chernov93@yandex.ru>
2020-04-23 00:37:00 +0300

    3.3. Разбор первой программы.[#278972]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ilia Chernov");
+        System.out.println("10.03.1993");
+    }
+}
