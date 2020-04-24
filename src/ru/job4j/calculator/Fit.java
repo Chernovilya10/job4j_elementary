03f013328dd52ed7748d2bd506a5dcc89cf4cfea
Chernovilya10 <i.chernov93@yandex.ru>
2020-04-24 18:40:46 +0300

    6. Идеальный вес.[#278953]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+        public static double manWeight(double height) {
+            double rsl = (height - 100) * 1.15; //ф-ла идеального веса для мужчин
+            return rsl;
+        }
+
+
+        public static double womanWeight(double height) {
+            double rsl = (height - 110) * 1.15; //ф-ла идеального веса для женщин
+            return rsl;
+        }
+
+        public static void main(String[] args) {
+            double man = Fit.manWeight(180);    //Вес мужчины при росте 180 см
+            System.out.println("Man 180 is " + man);
+            double woman = Fit.womanWeight(160);    //Вес женщины при росте 150 см
+            System.out.println("Woman 150 is " + woman);
+        }
+
+}
