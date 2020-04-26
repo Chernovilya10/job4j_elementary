fcc9660703c133c404b5f456c4b2ddc044525818
Chernovilya10 <i.chernov93@yandex.ru>
2020-04-26 18:55:25 +0300

    4.1. Операторы ветвлений[#278923]

---
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        posibleDiv(4);
+        posibleDiv(0);
+        posibleDiv(-9);
+    }
+    public static void posibleDiv(int number) {
+        if (number == 0) {
+            System.out.println("Could not div by 0.");
+        }
+        if (number < 0) {
+            System.out.println("This is negative numbers.");
+        }
+    }
+}
