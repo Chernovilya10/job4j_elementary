28629765a3c0f0ab78908e26c32108b6bb7772a8
Chernovilya10 <i.chernov93@yandex.ru>
2020-05-02 23:42:16 +0300

    5.5. Простое число[#279005]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,17 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;       //выполняется если число простое
-        if (number < 2) {          //любое число меньше 2 является непростым
-            prime = false;
-        } else {
-            for (int i = 2; i < (number - 1); i++) {
+        boolean prime = number >= 2;       //любое число меньше 2 является непростым
+            for (int i = 2; i < number; i++) {
                 if (number % i == 0) {    //если число делится не только на себя и единицу, то оно непростое
                     prime = false;
                     break;
                 }
             }
-        }
         return prime;
     }
 }
