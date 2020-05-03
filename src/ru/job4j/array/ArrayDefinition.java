c2d0d9e5b662386ca978f36ef1e9bdd1ef256d3a
Chernovilya10 <i.chernov93@yandex.ru>
2020-05-03 21:06:17 +0300

    6.0.2. Заполнение массива.[#278941]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Ilya Chernov";
+        names[1] = "Ivan Mikhailov";
+        names[2] = "Stepan Razin";
+        names[3] = "Fedor Emelianenko";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
