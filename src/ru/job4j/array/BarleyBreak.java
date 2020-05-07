c0f515c33625990cf58c92d19597b9ad8d4babbc
Chernovilya10 <i.chernov93@yandex.ru>
2020-05-07 17:43:36 +0300

    6.6.1. Двухмерный массив. Вывод на консоль.[#278947]

--- src/ru/job4j/array/BarleyBreak.java
+++ src/ru/job4j/array/BarleyBreak.java
@@ -2,12 +2,30 @@ package ru.job4j.array;
 
 public class BarleyBreak {
     public static void main(String[] args) {
-        int lengthP = 3;    //длина родительского массива
-        int lengthS = 3;    //длина дочернего массива
-        int[][] example = new int[lengthP][lengthS];
+//        int lengthP = 3;    //длина родительского массива
+//        int lengthS = 3;    //длина дочернего массива
+//        int[][] example = new int[lengthP][lengthS];
+//        int k = 0;
+//        for (int i = 0; i < lengthP; i++) {
+//            for (int j = 0; j < lengthS; j++) {
+//                example[i][j] = k++;
+//                System.out.print(example[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        /**
+         * Для любого количества элементов вложенных массивов с заданной lengthP (кол-во эл-ов родительского массива),
+         * если увеличиваем кол-во эл-ов родительского массива (lengthP),
+         * то необходимо определеить количество элементов для каждого нового вложенного массива
+         */
+        int lengthP = 3;
+        int[][] example = new int[lengthP][];
+        example[0] = new int[4];
+        example[1] = new int[2];
+        example[2] = new int[6];
         int k = 0;
         for (int i = 0; i < lengthP; i++) {
-            for (int j = 0; j < lengthS; j++) {
+            for (int j = 0; j < example[i].length; j++) {
                 example[i][j] = k++;
                 System.out.print(example[i][j] + " ");
             }
